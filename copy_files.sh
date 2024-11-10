1f92ce505c31cd9245b4207ad68d66711b91f5ab
Anders Tellefsen <anders1.tellefsen@gmail.com>
2024-11-10 20:16:03 +0000

    Created a working segmentation model

---
+++ copy_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through numbers 01 to 23
+for i in $(seq -w 1 23); do
+    dir="fish_$i"
+    if [ -d "$dir" ]; then
+        echo "Processing directory $dir"
+        # Copy all contents, including hidden files, to the current directory
+        cp -a "$dir"/. .
+        # Remove the subdirectory
+        rm -rf "$dir"
+    else
+        echo "Directory $dir does not exist."
+    fi
+done
